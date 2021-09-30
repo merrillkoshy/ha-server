480b1fd253f1c5677d64516197c5c139d8a13bc4
merrillkoshy <merrillkoshy@gmail.com>
2021-09-30 22:52:21 +0400

    added promise style pg

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ var options = {
 	// Initialization Options
 	promiseLib: promise,
 };
+require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
 var pgp = require("pg-promise")(options);
 var connectionString = isProduction
 	? process.env.DATABASE_URL
