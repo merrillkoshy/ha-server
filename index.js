d6d856d71bd47474ca566ecf8253ce748fb0d035
merrillkoshy <merrillkoshy@gmail.com>
2021-09-30 22:38:36 +0400

    added crud

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-// const {
-// 	getListings,
-// 	createListing,
-// 	deleteListing,
-// } = require("./Backend/pgCRUD");
+const { getListings, createListing, deleteListing } = require("./pgcrud");
 const cors = require("cors");
 
 app.use(cors());
@@ -14,33 +10,33 @@ app.use(express.json());
 app.get("/", (request, response) => {
 	response.json({ info: "Node.js, Express, and Postgres API" });
 });
-// app.get("/listing", (req, res) => {
-// 	try {
-// 		getListings(req, res);
-// 	} catch (error) {
-// 		res.status(500).send(error);
-// 	}
-// });
+app.get("/listing", (req, res) => {
+	try {
+		getListings(req, res);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
-// app.post("/listing", async (req, res) => {
-// 	createListing(req, res)
-// 		.then((response) => {
-// 			res.status(200).json(response);
-// 		})
-// 		.catch((error) => {
-// 			res.status(500).send(error);
-// 		});
-// });
+app.post("/listing", async (req, res) => {
+	createListing(req, res)
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
 
-// app.delete("/listing/:id", (req, res) => {
-// 	deleteListing(req.params.id)
-// 		.then((response) => {
-// 			res.status(200).send(response);
-// 		})
-// 		.catch((error) => {
-// 			res.status(500).send(error);
-// 		});
-// });
+app.delete("/listing/:id", (req, res) => {
+	deleteListing(req.params.id)
+		.then((response) => {
+			res.status(200).send(response);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
 app.listen(port, () => {
 	console.log(`App running on port ${port}.`);
 });
