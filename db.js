409c5a814ead301205328b654949b1e60a9748b7
merrillkoshy <merrillkoshy@gmail.com>
2021-10-01 01:02:15 +0400

    ssl

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ var promise = require("bluebird");
 var options = {
 	// Initialization Options
 	promiseLib: promise,
-	ssl: true,
 };
 require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
@@ -10,7 +9,9 @@ var pgp = require("pg-promise")(options);
 var connectionString = isProduction
 	? process.env.DATABASE_URL
 	: connectionString;
-var db = pgp(connectionString);
-pgp.pg.defaults.ssl = true;
+var db = pgp({
+	connectionString: connectionString,
+	ssl: true,
+});
 
 module.exports = db;
