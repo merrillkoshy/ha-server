4f29c1e1a8239325d4aad261c87813f075c3f2b5
merrillkoshy <merrillkoshy@gmail.com>
2021-10-03 10:38:35 +0400

    delete route clearance

--- index.js
+++ index.js
@@ -37,14 +37,11 @@ app.post("/listing", async (req, res) => {
 });
 
 app.delete("/listing/:id", (req, res) => {
-	deleteListing(req, res)
-		.then((res) => {
-			res.header("Access-Control-Allow-Origin", "*");
-			res.status(200).send(res);
-		})
-		.catch((error) => {
-			res.status(500).send(error);
-		});
+	try {
+		deleteListing(req, res);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 app.listen(port, () => {
