8b28cec63260e784dc101a665653a6cac83d2f87
merrillkoshy <merrillkoshy@gmail.com>
2021-10-03 08:36:38 +0400

    CRUD adjust

--- pgcrud.js
+++ pgcrud.js
@@ -8,7 +8,7 @@ const getListings = (req, res) => {
 		if (err) {
 			res.status(500).send(err.stack);
 		} else {
-			res.status(200).json(data);
+			res.status(200).json(data.rows[0]);
 		}
 	});
 };
@@ -42,7 +42,7 @@ const createListing = (req, res) => {
 		costs,
 	} = body;
 	const insertQuery =
-		"INSERT INTO hadata(id,price,currencyCode,address,type,kind,description,deposit,estimatedBills,minimumStayMonths,maxBookableDays,moveInWindow,currentOccupancy,rules,minAge,maxAge,preferredGender,alias,externalReference,extraData,facilities,calendarOperations,images,costs) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23 , $24)";
+		"INSERT INTO hadata(id,price,currencyCode,address,type,kind,description,deposit,estimatedBills,minimumStayMonths,maxBookableDays,moveInWindow,currentOccupancy,rules,minAge,maxAge,preferredGender,alias,externalReference,extraData,facilities,calendarOperations,images,costs) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23 , $24) RETURNING *";
 	client.query(
 		insertQuery,
 		[
@@ -76,7 +76,7 @@ const createListing = (req, res) => {
 				res.status(500).send(err.stack);
 			} else {
 				res.header("Access-Control-Allow-Origin", "*");
-				res.status(200).json(data);
+				res.status(200).json(data.rows[0]);
 			}
 		}
 	);
